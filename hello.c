6b33ae8f5bd59e182e2161bf0557fb4ce51b18bc
MukundWaghmare27 <mukundwaghmare27@gmail.com>
2024-12-08 01:00:18 +0530

    hello.c modified

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hellllllllllllloooooooooooooooooo from Jenkins\n");
+	printf("Hellllllllllllloooooooooooooooooo from Jenkins \n");
+	 printf("CICD Pipeline is working \n");
 
 	return 0;
 }
